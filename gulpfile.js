8a94257d3b30e45c04904e93fff1895ec5f42526
Matthew Jack <matthew.jack@sds.co.uk>
2021-05-26 22:48:19 +0100

    updated file path

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass')
 
 gulp.task('build', () => {
     return gulp
-        .src('src/**/*.scss')
+        .src('styles/src/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('dist'))
 })
