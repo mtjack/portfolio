d2a124d8cd6e762aa846ab8fb6fa72684656be09
Matthew Jack <matthew.jack@sds.co.uk>
2021-05-28 01:34:13 +0100

    added left/right col

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')
-
-gulp.task('build', () => {
-    return gulp
-        .src('styles/src/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist'))
-})
